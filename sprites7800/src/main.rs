463a8e29675bf94a2830d9b824aec9782c34069f
Bruno STEUX <B.STEUX@nexter-group.fr>
2023-07-03 19:23:31 +0200

    sparse tiling starting to work

--- sprites7800/src/main.rs
+++ sprites7800/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::error::Error;
 use serde::Deserialize;
 use clap::Parser;
 use image::GenericImageView;
@@ -13,15 +14,23 @@ struct Args {
 
 #[derive(Debug, Deserialize)]
 struct AllSprites {
+    #[serde(default)]
+    palettes: Option<Vec<Palette>>,
     sprite_sheets: Vec<SpriteSheet>
 }
 
 #[derive(Debug, Deserialize)]
 struct SpriteSheet {
     image: String,
-    #[serde(default = "default_resolution")]
-    resolution: String,
+    #[serde(default = "default_mode")]
+    mode: String,
     sprites: Vec<Sprite>
+}
+
+#[derive(Debug, Deserialize)]
+struct Palette {
+    name: String,
+    colors: Vec<(u8, u8, u8)>
 }
 
 #[derive(Debug, Deserialize)]
@@ -34,37 +43,75 @@ struct Sprite {
     holeydma: bool,
     #[serde(default = "default_sprite_size")]
     height: u32,
-    #[serde(default = "default_color")]
-    color1: (u8, u8, u8),
-    #[serde(default = "default_color")]
-    color2: (u8, u8, u8),
-    #[serde(default = "default_color")]
-    color3: (u8, u8, u8),
+    #[serde(default)]
+    palette: Option<String>,
+    #[serde(default)]
+    mode: Option<String>,
 }
 
 fn default_sprite_size() -> u32 { 16 }
 fn default_holeydma() -> bool { true }
-fn default_resolution() -> String { "160A".to_string() }
-fn default_color() -> (u8, u8, u8) { (0, 0, 0) }
+fn default_mode() -> String { "160A".to_string() }
 
-fn main() -> Result <(), serde_yaml::Error> {
+// Color tables:
+//
+// | mode | colors |
+// | 160A | PXC1, PXC2, PXC3
+// | 160B | P2 = 0 => P0C1, P0C2, P0C3, P1C1, P1C2, P1C3, P2C1, P2C2, P2C3, P3C1, P3C2, P3C3
+// |      | P2 = 1 => P4C1, P4C2, P4C3, P5C1, P5C2, P5C3, P6C1, P6C2, P6C3, P7C1, P7C2, P7C3
+// | 320A | PXC2
+// | 320B | P2 = 0 => P0C1, P0C2, P0C3
+// |      | P2 = 1 => P4C1, P4C2, P4C3
+// | 320C | P2 = 0 => P0C2, P1C2, P2C2, P3C2
+// |      | P2 = 1 => P4C2, P5C2, P6C2, P7C2
+//
+
+fn main() -> Result <(), Box<dyn Error>> {
     let args = Args::parse();
     let contents = fs::read_to_string(args.filename).expect("Unable to read input file");
     let all_sprites: AllSprites = serde_yaml::from_str(&contents)?;
     for sprite_sheet in all_sprites.sprite_sheets {
         let img = image::open(&sprite_sheet.image).expect(&format!("Can't open image {}", sprite_sheet.image));
+
         for sprite in sprite_sheet.sprites {
-            let mut colors = [sprite.color1, sprite.color2, sprite.color3];
-            let mut bytes = Vec::<u8>::new();
-            let pixel_width = match sprite_sheet.resolution.as_str() {
-                "320A" | "320B" => 1,
+            let mode = if let Some(s) = &sprite.mode { s.as_str() } else {
+                sprite_sheet.mode.as_str()
+            }; 
+
+            let pixel_width = match mode {
+                "320A" | "320B" | "320C" | "320D" => 1,
                 _ => 2,
             };
-            let pixel_bits = match sprite_sheet.resolution.as_str() {
-                "320A" => 1,
+            let pixel_bits = match mode {
+                "320A" | "320D" => 1,
                 "160B" => 4,
                 _ => 2,
             };
+            let maxcolors = match mode {
+                "160A" => 3,
+                "160B" => 12,
+                "320A" => 1,
+                "320B" => 3,
+                "320C" => 4,
+                "320D" => 1,
+                _ => return Err(Box::new(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("Unknown gfx {} mode", mode))))
+            };
+            
+            let mut colors = [(0u8, 0u8, 0u8);12];
+            if let Some(palettes) = &all_sprites.palettes {
+                if let Some(pname) = sprite.palette {
+                    let px = palettes.into_iter().find(|x| x.name == pname);
+                    if let Some(p) = px { 
+                        let mut i = 0;
+                        for c in &p.colors {
+                            colors[i] = *c;
+                            i += 1;
+                        }
+                    }
+                }
+            }
+
+            let mut bytes = Vec::<u8>::new();
             for y in 0..sprite.height {
                 let mut current_byte: u8 = 0;
                 let mut current_bits: u8 = 0;
@@ -74,7 +121,17 @@ fn main() -> Result <(), serde_yaml::Error> {
                     if color[3] == 0 || (color[0] == 0 && color[1] == 0 && color[2] == 0) {
                         cx = Some(0); // Background color (either black or transparent)
                     } else {
-                        for c in 0..3 {
+                        if mode == "320C" {
+                            // Check next pixel, should be background or same color
+                            let colorr = img.get_pixel(sprite.left + x * pixel_width + 1, sprite.top + y);
+                            if !(colorr[3] == 0 || (colorr[0] == 0 && colorr[1] == 0 && colorr[2] == 0)) {
+                                // This is not background
+                                if colorr != color {
+                                    return Err(Box::new(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("Two consecutive pixels have a different color in 320C mode (x = {}, y = {})", x * 2, y))));
+                                }
+                            }
+                        }
+                        for c in 0..maxcolors {
                             if color[0] == colors[c].0 && color[1] == colors[c].1 && color[2] == colors[c].2 {
                                 // Ok. this is a pixel of color c
                                 cx = Some((c + 1) as u8);
@@ -83,7 +140,7 @@ fn main() -> Result <(), serde_yaml::Error> {
                         }
                         if cx.is_none() {
                             // Let's find a unaffected color
-                            for c in 0..3 {
+                            for c in 0..maxcolors {
                                 if colors[c].0 == 0 && colors[c].1 == 0 && colors[c].2 == 0 {
                                     colors[c].0 = color[0];
                                     colors[c].1 = color[1];
@@ -93,19 +150,83 @@ fn main() -> Result <(), serde_yaml::Error> {
                                 }
                             }
                             if cx.is_none() {
-                                panic!("Sprite {} has more than 3 colors", sprite.name);
+                                panic!("Sprite {} has more than {} colors", sprite.name, maxcolors);
                             }
                         }
                     }
-                    current_byte |= cx.unwrap();
-                    current_bits += pixel_bits;
-                    if current_bits == 8 {
-                        bytes.push(current_byte);
-                        current_byte = 0;
-                        current_bits = 0;
-                    } else {
-                        current_byte <<= pixel_bits;
-                    }
+                    match mode {
+                        "160A" | "320A" | "320D" => {
+                            current_byte |= cx.unwrap();
+                            current_bits += pixel_bits;
+                            if current_bits == 8 {
+                                bytes.push(current_byte);
+                                current_byte = 0;
+                                current_bits = 0;
+                            } else {
+                                current_byte <<= pixel_bits;
+                            };
+                        },
+                        "160B" => {
+                            let c = match cx.unwrap() {
+                                0 => 0,
+                                1 => 1,
+                                2 => 2,
+                                3 => 3,
+                                4 => 5,
+                                5 => 6,
+                                6 => 7,
+                                7 => 9,
+                                8 => 10,
+                                9 => 11,
+                                10 => 13,
+                                11 => 14,
+                                12 => 15,
+                                _ => 0
+                            };
+                            current_byte |= (if c & 1 != 0 { 16 } else { 0 }) |
+                                            (if c & 2 != 0 { 32 } else { 0 }) |
+                                            (if c & 4 != 0 { 1 } else { 0 }) |
+                                            (if c & 8 != 0 { 2 } else { 0 });
+                            current_bits += 1;
+                            if current_bits == 2 {
+                                bytes.push(current_byte);
+                                current_byte = 0;
+                                current_bits = 0;
+                            } else {
+                                current_byte <<= 2;
+                            };
+                        },
+                        "320B" => {
+                            let c = cx.unwrap();
+                            current_byte |= (if c & 1 != 0 { 1 } else { 0 }) |
+                                            (if c & 2 != 0 { 16 } else { 0 });
+                            current_bits += 1;
+                            if current_bits == 4 {
+                                bytes.push(current_byte);
+                                current_byte = 0;
+                                current_bits = 0;
+                            } else {
+                                current_byte <<= 1;
+                            };
+                        },
+                        "320C" => {
+                            let c = cx.unwrap();
+                            if c != 0 {
+                                current_byte |= 1 << (7 - current_bits);
+                                if current_bits < 2 {
+                                    current_byte |= c << 2;
+                                } else {
+                                    current_byte |= c;
+                                }
+                            }
+                            current_bits += 1;
+                            if current_bits == 4 {
+                                bytes.push(current_byte);
+                                current_byte = 0;
+                                current_bits = 0;
+                            }                        },
+                        _ => unreachable!(),
+                    };
                 }
             }
             // Whoaw. We do have our pixels vector. Let's output it
--- tiles7800/src/main.rs
+++ tiles7800/src/main.rs
@@ -2,8 +2,9 @@ use std::fs;
 use std::error;
 use clap::Parser;
 use std::str::FromStr;
-
+use serde::Deserialize;
 use xml_dom::level2::{Node, NodeType};
+use std::collections::HashMap;
 
 /// Atari 7800 tool that generates C code for tiles map generated using tiled editor (tmx files) 
 #[derive(Parser, Debug)]
@@ -12,18 +13,92 @@ struct Args {
     /// Generate 0xff boundaries
     #[arg(short, long, default_value = "false")]
     boundaries: bool,
-    /// YAML input file
+    /// Tiled input file (.TMX file)
     filename: String,
+    /// Sparse tiling code generation (provide yaml file)
+    #[arg(long = "sparse")]
+    yaml: Option<String>
 }
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct AllSprites {
+    #[serde(default)]
+    palettes: Option<Vec<Palette>>,
+    sprite_sheets: Vec<SpriteSheet>
+}
+
+#[derive(Deserialize)]
+struct SpriteSheet {
+    image: String,
+    #[serde(default = "default_mode")]
+    mode: String,
+    sprites: Vec<Sprite>
+}
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Palette {
+    name: String,
+    colors: Vec<(u8, u8, u8)>
+}
+
+#[allow(unused)]
+#[derive(Deserialize)]
+struct Sprite {
+    name: String,
+    top: u32,
+    left: u32,
+    width: u32,
+    #[serde(default = "default_holeydma")]
+    holeydma: bool,
+    #[serde(default = "default_sprite_size")]
+    height: u32,
+    #[serde(default)]
+    palette: Option<String>,
+    #[serde(default)]
+    mode: Option<String>,
+    #[serde(default)]
+    palette_number: Option<u8>
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Tile<'a> {
+    index: u32,
+    mode: &'a str,
+    palette_number: u8,
+}
+
+fn default_sprite_size() -> u32 { 16 }
+fn default_holeydma() -> bool { true }
+fn default_mode() -> String { "160A".to_string() }
 
 fn main() -> Result <(), Box<dyn error::Error>>
 {
     let mut width = 0;
     let mut height = 0;
+    let mut tilewidth: u32 = 8;
+    let mut tileheight: u32 = 8;
     let args = Args::parse();
     let xml = fs::read_to_string(args.filename).expect("Unable to read input file");
     let dom = xml_dom::parser::read_xml(&xml)?;
     let root = dom.first_child().unwrap();
+    if root.local_name() == "map" {
+        for a in &root.attributes() {
+            if a.0.local_name() == "tileheight" {
+                let h = a.1.first_child().unwrap().node_value();
+                if let Some(s) = h {
+                    tileheight = s.parse::<u32>()?;
+                }
+            }
+            if a.0.local_name() == "tilewidth" {
+                let h = a.1.first_child().unwrap().node_value();
+                if let Some(s) = h {
+                    tilewidth = s.parse::<u32>()?;
+                }
+            }
+        }
+    }
     for n in &root.child_nodes() {
         if n.node_type() == NodeType::Element {
             if n.local_name() == "layer" {
@@ -52,28 +127,168 @@ fn main() -> Result <(), Box<dyn error::Error>>
                             //println!("Tiles: {}", csv);
                             let array = csv.split(',').map(|x| { u32::from_str(x).unwrap() } ).collect::<Vec::<_>>();
                             if array.len() == width * height {
-                                print!("const char tilemap[{}] = {{", 
-                                    if args.boundaries {
-                                        (width + 1) * height + 1
-                                    } else {
-                                        width * height
-                                    });
-                                for i in 0..height {
-                                    if args.boundaries { 
-                                        print!("\n\t0xff, "); 
-                                    } else {
-                                        print!("\n\t");
+                                if let Some(yaml_file) = args.yaml {
+                                    let contents = fs::read_to_string(yaml_file).expect("Unable to read input file");
+                                    let t: AllSprites = serde_yaml::from_str(&contents)?;
+                                    // OK, we have the array, we have the tiles specs. Let's match them
+                                    // Let's scan all the tiles to make sure all this makes sense
+                                    if t.sprite_sheets.len() != 1 {
+                                        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Only one sprite sheet (tiles) should be provided")));
                                     }
-                                    for j in 0..width {
-                                        print!("{}{} ", (array[i * height + j] - 1) * 2,
-                                        if args.boundaries || i != height - 1 || j != width - 1 {","} else {""}
-                                        );
+                                    let tiles_sheet = &t.sprite_sheets[0];
+                                    let img = image::open(&tiles_sheet.image).expect(&format!("Can't open image {}", tiles_sheet.image));
+                                    let image_width = img.width();
+                                    let mut index = 0;
+                                    let defmode = tiles_sheet.mode.as_str();
+                                    let mut tiles = HashMap::<u32, Tile>::new();
+                                    for tile in &tiles_sheet.sprites {
+                                        // Do not consider sprites
+                                        let mode = if let Some(m) = &tile.mode { m.as_str() } else { defmode };
+                                        let tile_bytes = match mode {
+                                            "160A" => tilewidth / 8,
+                                            "160B" => tilewidth / 4,
+                                            "320A" => tilewidth / 8,
+                                            "320B" => tilewidth / 4,
+                                            "320C" => tilewidth / 4,
+                                            _ => unreachable!()
+                                        };
+                                        if !tile.holeydma {
+                                            let y = tile.top / tileheight;
+                                            let x = tile.left / tilewidth;
+                                            let ix = 1 + x + y * image_width / tilewidth;
+                                            let nbtiles = tile.width / tilewidth;
+                                            let palette_number = if let Some(p) = tile.palette_number { p } else { 0 }; 
+                                            for i in 0..nbtiles {
+                                                tiles.insert(ix + i, Tile {
+                                                    index, mode, palette_number
+                                                });
+                                                index += tile_bytes;
+                                            }
+                                        }
                                     }
-                                }
-                                if args.boundaries { 
-                                    println!("\n\t0xff}};");
+                                    //println!("Tiles : {:?}", tiles);
+
+                                    // Generate the C code for the the sparse tiles
+                                    // to be used with sparse_tiling.h header
+                                    for y in 0..height {
+                                        // For each line, find the tilesets 
+                                        let mut tilesets = Vec::<(u32, Vec::<Tile>)>::new();
+                                        let mut tileset = Vec::<Tile>::new();
+                                        let mut startx = 0;
+                                        for x in 0..width {
+                                            let cell = array[y * height + x];
+                                            if cell == 0 {
+                                                if !tileset.is_empty() {
+                                                    tilesets.push((startx, tileset));
+                                                    tileset = Vec::<Tile>::new();
+                                                }
+                                            } else {
+                                                if let Some(tx) = tileset.last() {
+                                                    // Is the cell compatible with the tileset in construction ?
+                                                    if let Some(t) = tiles.get(&cell) {
+                                                        if t.mode == tx.mode && t.palette_number == tx.palette_number {
+                                                            // Yes, let's add it the current tileset
+                                                            if tileset.len() >= (256 - 160)/ 8 {
+                                                                tilesets.push((startx, tileset));
+                                                                tileset = Vec::<Tile>::new();
+                                                                startx = x as u32;
+                                                            }
+                                                            tileset.push(t.clone());
+                                                        } else {
+                                                            tilesets.push((startx, tileset));
+                                                            tileset = Vec::<Tile>::new();
+                                                            tileset.push(t.clone());
+                                                            startx = x as u32;
+                                                        }
+                                                    } else {
+                                                        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Wrong tilesheet. Index unknown")));
+                                                    }
+                                                } else {
+                                                    if let Some(t) = tiles.get(&cell) {
+                                                        tileset.push(t.clone());
+                                                        startx = x as u32;
+                                                    } else {
+                                                        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Wrong tilesheet. Index unknown")));
+                                                    }
+                                                }
+                                            }           
+                                        }
+                                        // Write the last tileset
+                                        if !tileset.is_empty() {
+                                            tilesets.push((startx, tileset));
+                                        }
+                                        // Write this line data
+                                        {
+                                            let mut c = 0;
+                                            let mut w = Vec::new();
+                                            for s in &tilesets {
+                                                let mut tn = Vec::new();
+                                                for t in &s.1 {
+                                                    // We assume we are in 2 bytes mode
+                                                    let nb = match t.mode {
+                                                        "160A" | "320A" => tilewidth / 16,
+                                                        _ => tilewidth / 8,
+                                                    };
+                                                    for i in 0..nb {
+                                                        tn.push(t.index + i * 2);
+                                                    }
+                                                }
+                                                w.push(tn.len());
+                                                print!("const char tilemap_{}_{}[{}] = {{", y, c, tn.len());
+                                                for i in 0..tn.len() - 1 {
+                                                    print!("{}, ", tn[i]);
+                                                }
+                                                println!("{}}};", tn[tn.len() - 1]);
+                                                c += 1;
+                                            }
+                                            c = 0;
+                                            print!("const char tilemap_{}_data[] = {{", y);
+                                            for s in &tilesets {
+                                                let ttype = s.1.first().unwrap();
+                                                let write_mode = match ttype.mode {
+                                                    "160A" | "320A" | "320D" => 0x60,
+                                                    _ => 0xE0,
+                                                };
+                                                print!("{}, {}, tilemap_{y}_{c}, 0x{:02x}, tilemap_{y}_{c} >> 8, ({} << 5) | ((-{}) & 0x1f), {}, ", 
+                                                    s.0 + s.1.len() as u32 - 1, s.0, write_mode, ttype.palette_number, w[c], (10 + 3 + 9 * w[c]) / 2);
+                                                c += 1;
+                                            }
+                                            println!("0x7f, 0xff}};");
+                                        }
+                                    }
+                                    print!("const char *tilemap_data[{}] = {{", height);
+                                    for y in 0..height - 1 {
+                                        print!("tilemap_{}_data, ", y);
+                                    }
+                                    println!("tilemap_{}_data}};\n", height - 1);
+                                    println!("#define TILING_HEIGHT {}", height);
+                                    println!("#define TILING_WIDTH {}", width);
+                                    println!("#include \"sparse_tiling.h\"\n");
+
                                 } else {
-                                    println!("\n\t}};");
+                                    print!("const char tilemap[{}] = {{", 
+                                        if args.boundaries {
+                                            (width + 1) * height + 1
+                                        } else {
+                                            width * height
+                                        });
+                                    for i in 0..height {
+                                        if args.boundaries { 
+                                            print!("\n\t0xff, "); 
+                                        } else {
+                                            print!("\n\t");
+                                        }
+                                        for j in 0..width {
+                                            print!("{}{} ", (array[i * height + j] - 1) * 2,
+                                            if args.boundaries || i != height - 1 || j != width - 1 {","} else {""}
+                                            );
+                                        }
+                                    }
+                                    if args.boundaries { 
+                                        println!("\n\t0xff}};");
+                                    } else {
+                                        println!("\n\t}};");
+                                    }
                                 }
                                 return Ok(());
                             } else {
